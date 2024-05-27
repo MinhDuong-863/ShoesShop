c238ee8dbb2f455d5ca29f57a5ca22d972e2f032
MinhDuong <luceattran@gmail.com>
2024-05-27 14:49:19 +0700

    SS01 | Model

---
+++ src/main/java/com/project/shopapp/models/Product.java
@@ -0,0 +1,32 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product extends BaseModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Float price;
+
+    private String thumbnail;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category categoryId;
+}
---
+++ src/main/java/com/project/shopapp/models/Token.java
@@ -0,0 +1,34 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tokens")
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String token;
+
+    @Column(name = "token_type")
+    private String tokenType;
+
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+    private boolean revoked;
+    private boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
